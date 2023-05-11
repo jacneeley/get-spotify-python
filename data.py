549208847a468fd3e55a7a265325ee48af4bae6a
Neeley <jcn73@txstate.edu>
2023-05-11 09:50:40 -0500

    added conditional "path"

--- data.py
+++ data.py
@@ -2,9 +2,14 @@ import pandas as pd
 from dotenv import load_dotenv
 import os
 
-# load_dotenv()
-# path = os.getenv("PATH")
-path = 'C:/Users/jcn73/Documents/python-scripts/NACC_VIEW_CHARTS_EXPORT_20230501.csv'
+load_dotenv()
+path = os.getenv("FILE_PATH")
+
+if(path == None):
+    print("No path provided in .env")
+    path = str(input("please provide a file path: "))
+    path.replace("\\","/")
+
 df = pd.read_csv(path)
 
 artists = []
